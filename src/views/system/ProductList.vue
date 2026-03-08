d102f3a8181b035bf6f757732f93ea79d2f2cae3
duffy <duffydeng@163.com>
2026-03-08 16:56:20 +0800

    分类 页面add

---
+++ src/views/system/ProductList.vue
@@ -0,0 +1,15 @@
+<template>
+  <product-category-list />
+</template>
+
+<script>
+  import ProductCategoryList from '@/views/product/ProductCategoryList'
+
+  export default {
+    name: 'ProductList',
+    components: {
+      ProductCategoryList
+    }
+  }
+</script>
+
